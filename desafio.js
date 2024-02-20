cc649bb619edb963ebcc0ee03f5577965fe9e145
Naia <nai.caparelli@gmail.com>
2024-02-20 14:43:20 -0300

    Feat: My first challenge javascript. A travel fuel calculator.

--- desafio.js
+++ desafio.js
@@ -1,7 +1,9 @@
 /* Faça um programa para calcular o valor de uma viagem.
 
-Você terá 3 variáveis. Sendo elas
-    1 - Preço do combustível;
+Você terá 5 variáveis. Sendo elas:
+    1 - Preço da gasolina;
+    2 - Preco do etanol;
+    3 - Tipo de combustivel;
     2 - Gasto médio de combustível do carro por KM;
     3 - Distância em KM da viagem;
 
